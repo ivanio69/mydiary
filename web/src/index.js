d438eea897c5c49fd965f03aa71acb50c7321fa0
lifecats <lifecatsup@mail.ru>
2020-08-16 16:42:10 +0300

    r e f a c t o r

--- web/src/index.js
+++ web/src/index.js
@@ -33,8 +33,8 @@ ReactDOM.render(
         <Route exact path="/">
           <Home />
         </Route>
-      )}
-      <Route exact path="*">
+      ) }
+      <Route exact path="*" status={404}>
         <Result
           status="404"
           title="404"
@@ -46,7 +46,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
