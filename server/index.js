2dc8ad4f206b639e60a23a6e79e6030b978a4914
lifecats <lifecatsup@mail.ru>
2021-02-06 20:57:59 +0300

    v2.0.0

--- server/index.js
+++ server/index.js
@@ -1,296 +1,196 @@
-// Mydiary API V1
-// Creative Commons License  at ./LICENSE.md
-
-const { v4: uuidv4 } = require("uuid");
-const path = require("path");
 const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const md5 = require("md5");
 const app = express();
-app.use(bodyParser.json());
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+const uuid = require("uuid").v4;
+dotenv.config();
+const secret = "appSecretKey";
+const rounds = 9921;
+const keySize = 32;
+const algorithm = "aes-256-cbc";
+const salt = crypto.createHash("sha1").update(secret).digest("hex");
+const jwtSecret = process.env.JWTSECRET;
 mongoose.connect("mongodb://localhost:27017/mydiary", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
-  console.log("Connected to DB...");
-  const schema = new mongoose.Schema({
-    name: String,
-    pass: String,
-    email: String,
-    notes: Array,
-    registered: String,
-    emailUpdates: Boolean,
-  });
-  const User = mongoose.model("User", schema);
+const User = mongoose.model("User", {
+  name: String,
+  email: String,
+  password: String,
+  posts: Array,
+  followers: Array,
+});
 
-  //actual version
-  app.get("/api/ver", (req, res) => {
-    res.json({ version: "0.1.0" });
-  });
+function encrypt(data) {
+  let iv = crypto.randomBytes(16);
+  let key = crypto.pbkdf2Sync(secret, salt, rounds, keySize, "sha512");
+  let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
+  let encryptedData = Buffer.concat([
+    cipher.update(JSON.stringify(data)),
+    cipher.final(),
+  ]);
+  return { iv: iv.toString("base64"), data: encryptedData.toString("base64") };
+}
 
-  app.use(express.static(path.join(__dirname, "/../web/build")));
+app.use(cors());
+app.use(bodyParser.json());
 
-  // V1
-  //new account
-  app.post("/api/v1/register", (req, res) => {
-    var today = new Date();
-    var date =
-      today.getFullYear() +
-      "-" +
-      (today.getMonth() + 1) +
-      "-" +
-      today.getDate();
-    const user = new User({
-      name: req.body.name,
-      email: req.body.email,
-      pass: md5(req.body.pass),
-      notes: [],
-      registered: date,
-      emailUpdates: req.body.emailUpdates,
+app.get("/", (req, res) => {
+  res.send("well, hi :)");
+});
+
+app.post("/api/v1/login", (req, res) => {
+  if (req.body.email && req.body.password) {
+    User.findOne({ email: req.body.email }, (err, user) => {
+      if (user) {
+        bcrypt.compare(req.body.password, user.password, (err, same) => {
+          delete user.password;
+          if (same)
+            jwt.sign({ user }, jwtSecret, (err, token) => {
+              res.json({ status: 1, user, token });
+            });
+          else res.json({ status: 0, message: "Email or password is wrong!" });
+        });
+      } else res.json({ status: 0, message: "Email or password is wrong!" });
     });
-    try {
-      User.findOne({ email: req.body.email }, function (err, resp) {
-        if (err) {
-          res.json({ message: err.message, err });
-        } else {
-          if (resp === null) {
-            user.save(function (err, data) {
-              if (err) return console.error(err);
-              res.send({ data, status: 1, message: "Registered sucsessfully" });
+  }
+});
+
+app.post("/api/v1/register", (req, res) => {
+  if (req.body.password && req.body.email && req.body.name) {
+    User.findOne({ email: req.body.email }, (err, exists) => {
+      if (exists)
+        res.json({ status: 0, message: "This email is already registered!" });
+      else
+        bcrypt.hash(req.body.password, 15, function (err, hashedPass) {
+          const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPass,
+            posts: [],
+            followers: [],
+          });
+
+          newUser.save((err, user) => {
+            delete user.password;
+            jwt.sign({ user }, jwtSecret, (err, token) => {
+              res.json({ status: 1, user, token });
             });
-          } else res.json({ message: "Email is alredy in use!" });
-        }
-      });
-    } catch (err) {
-      console.error("POST/register error: ", err);
-      res.json({
-        message: err.message,
-        err,
-      });
-    }
-  });
+          });
+        });
+    });
+  } else res.json({ status: 0, message: "There is not enough data!" });
+});
 
-  //login
-  app.post("/api/v1/login", (req, res) => {
-    try {
-      User.findOne(
-        { email: req.body.email, pass: md5(req.body.pass) },
-        function (err, data) {
-          if (err) {
-            res.json({ message: err.message, err });
+app.post("/api/v1/getNote", (req, res) => {
+  User.findOne({ "posts.uuid": req.body.id }, (err, user) => {
+    if (user) {
+      res.json({
+        status: 1,
+        note: user.posts.filter((e) => e.uuid === req.body.id)[0],
+        user,
+      });
+    } else res.json({ status: 0, message: "This note not found!" });
+  });
+});
+
+app.use((req, res, next) => {
+  if (req.headers.token) {
+    jwt.verify(req.headers.token, jwtSecret, (err, userData) => {
+      if (err)
+        res.json({
+          status: 3,
+          message:
+            "There was an error decrypting token! Please clear your cache.",
+          err,
+        });
+      else
+        User.findOne({ email: userData.user.email }, (err, exists) => {
+          if (exists) {
+            req.user = exists;
+            req.user.password = undefined;
+
+            next();
           } else {
-            if (data === null)
-              res.json({
-                status: 0,
-                message: "Password or email is incorrect!",
-              });
-            else res.send({ data, message: "Logged in!", status: 1 });
+            res.json({ status: 3, message: "This account does not exist!" });
           }
-        }
-      );
-    } catch (err) {
-      console.error("POST/login error: ", err);
-      res.json({
-        message: err.message,
-        err,
-      });
-    }
-  });
+        });
+    });
+  } else {
+    res.json({ status: 3, message: "No token header presented" });
+  }
+});
 
-  //get user data
-  app.post("/api/v1/userdata", (req, res) => {
-    try {
-      User.findOne({ _id: req.body.id }, function (err, data) {
-        if (err) {
-          res.json({ message: err.message, err });
-        } else {
-          res.json(data);
-        }
-      });
-    } catch (err) {
-      console.error("POST/login error: ", err);
-      res.json({
-        message: err.message,
-        err,
-      });
-    }
-  });
+app.get("/api/v1/user", (req, res) => {
+  res.json(req.user);
+});
 
-  app.post("/api/v1/rmaccount", (req, res) => {
-    try {
-      User.deleteOne({ email: req.body.email }, (err, resp) => {
-        if (err) {
-          res.json({ message: err.message, err });
-        } else {
-          res.send(resp);
-        }
-      });
-    } catch (err) {
-      console.error("GET/rmaccount error: ", err);
-      res.json({
-        message: err.message,
-        err,
-      });
-    }
-  });
-
-  app.post("/api/v1/addpost", (req, res) => {
-    const id = uuidv4();
-    let a = {};
-    a.snippet = "";
-
-    // the for loop was repeating forever since s kept getting reassigned.  String.replace() with the RegEx seems to work
-    const s = req.body.text.replace(/[#`*\[\]\{\}_\\\(\)+-.!]/g, "");
-
-    if (s.length < 50) {
-      a.snippet = s;
-    } else {
-      for (let i = 0; i < 50; i++) {
-        a.snippet += s.charAt(i);
-      }
-      a.snippet += "...";
-    }
-    try {
-      User.updateOne(
-        { email: req.body.email },
-        {
-          $push: {
-            notes: {
-              email: req.body.email,
-              user_id: req.body.user_id,
-              uname: req.body.uname,
-              name: req.body.name,
-              snippet: a.snippet,
-              text: req.body.text,
-              id,
-            },
-          },
+app.post("/api/v1/createNote", (req, res) => {
+  const id = uuid();
+  User.updateOne(
+    req.user,
+    {
+      $push: {
+        posts: {
+          name: req.body.noteName,
+          body: req.body.noteBody,
+          uuid: id,
         },
-        (err, resp) => {
-          if (err) {
-            res.json({ message: err.message, err });
-          } else {
-            res.send({
-              user_id: req.body.user_id,
-              email: req.body.email,
-              uname: req.body.uname,
-              name: req.body.name,
-              snippet: a.snippet,
-              text: req.body.text,
-              id,
-              status: 1,
-              message: "Saved!",
-            });
-          }
-        }
-      );
-    } catch (err) {
-      console.error("POST/addpost error: ", err);
+      },
+    },
+    (err, updated) => {
       res.json({
-        message: err.message,
-        err,
-      });
-    }
-  });
-
-  app.post("/api/v1/editpost", (req, res) => {
-    try {
-      User.update(
-        { "notes.id": req.body.id },
-        {
-          $set: {
-            "notes.$.name": req.body.name,
-            "notes.$.text": req.body.text,
-          },
+        status: 1,
+        note: {
+          name: req.body.noteName,
+          body: req.body.noteBody,
+          uuid: id,
         },
-        (err, resp) => {
-          if (err) {
-            res.json({ message: err.message, err });
-          } else {
-            res.send(resp);
-          }
-        }
-      );
-    } catch (err) {
-      console.error("POST/editpost error: ", err);
-      res.json({
-        message: err.message,
-        err,
       });
     }
-  });
+  );
+});
 
-  app.post("/api/v1/rmpost", (req, res) => {
-    try {
-      User.updateOne(
-        { email: req.body.email },
-        { $pull: { notes: { id: req.body.id } } },
-        (err, resp) => {
-          if (err) {
-            res.json({ message: err.message, err });
-          } else {
-            res.send(resp);
-          }
-        }
-      );
-    } catch (err) {
-      console.error("POST/rmpost error: ", err);
+app.post("/api/v1/removeNote", (req, res) => {
+  User.updateOne(
+    { "posts.uuid": req.body.uuid, email: req.user.email },
+    {
+      $pull: {
+        posts: {
+          uuid: req.body.uuid,
+        },
+      },
+    },
+    (err, updated) => {
       res.json({
-        message: err.message,
-        err,
+        status: 1,
+        updated,
       });
     }
-  });
+  );
+});
 
-  app.post("/api/v1/notes", (req, res) => {
-    try {
-      User.findOne({ email: req.body.email }, (err, u) => {
-        if (err) {
-          res.json({ message: err.message, err });
-        } else {
-          if (u == null)
-            res.json({ message: "Your account has been removed!", status: 69 });
-          else res.send(u.notes);
-        }
-      });
-    } catch (err) {
-      console.error("POST/notes error: ", err);
-      res.json({
-        message: err.message,
-        err,
-      });
+app.post("/api/v1/updateNote", (req, res) => {
+  User.updateOne(
+    { "posts.uuid": req.body.uuid, email: req.user.email },
+    {
+      $set: {
+        "posts.$.name": req.body.noteName,
+        "posts.$.body": req.body.noteBody,
+      },
+    },
+    (err, updated) => {
+      res.json({ status: 1, updated });
     }
-  });
+  );
+});
 
-  app.post("/api/v1/getnote", (req, res) => {
-    try {
-      User.findOne({ "notes.id": req.body.id }, (err, u) => {
-        if (err) {
-          res.json({ message: err.message, err });
-        } else {
-          if (u == null) {
-            res.sendStatus(404);
-          } else res.send(u.notes.find((x) => x.id === req.body.id));
-        }
-      });
-    } catch (err) {
-      console.error("POST /getnote error: ", err);
-      res.json({
-        message: err.message,
-        err,
-      });
-    }
-  });
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../web/build/index.html"));
-  });
-
-  app.listen(8081, () => {
-    console.log("Sevrer started!");
-  });
+app.listen(3584, () => {
+  console.info("Running on port 3584");
 });
